686467961eee3b7d2965ea5d62e2b494fe5d4866
fkbeys <fkbeys@gmail.com>
2023-06-13 09:15:26 +0200

    Add project files.

---
+++ ValueVsReferance/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ValueVsReferance
+{
+    internal class Program
+    {
+
+        static ref int calc(ref int num)
+        {
+            num = num + 1;
+            return ref num;
+        }
+
+        public static Student ChangeName(Student stu)
+        {
+            //stu=new Student();
+            stu.name = "changed";
+            return new Student { id = 33, name = "xx" };
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            string a = "a";
+            string b = a;
+            b = "c";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+
+        }
+    }
+}
---
+++ ValueVsReferance/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace ValueVsReferance
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public struct StudentStruct
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+}
